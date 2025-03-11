68ec9e19e39cccb3681605071a19f3c18a280811
Saanya <saanya1000@gmail.com>
2025-03-11 21:03:54 +0530

    Modified

---
+++ EvenOdd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a;
+    printf("Enter a number of your choice:");
+    scanf("%d",&a);
+    if (a/2 ==0)
+    {
+        printf("The number chosen by you is even.");
+    }
+    else{
+        printf("The number Chosen By you is even.");
+    }
+    return 0;
+}

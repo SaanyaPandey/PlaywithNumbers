071784c97f8f721cc24d44037897904d656949c7
Saanya <saanya1000@gmail.com>
2025-03-12 05:57:35 +0530

    This is program to find even or odd number using cpp language.

---
+++ EvenOdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter a Number of Your Choice: ";
+	cin>>a;
+	if(a%2 == 0)
+	{
+		cout<<"The number is even.";
+	}
+	else 
+	{
+		cout<<"The number is odd";
+	}
+}
